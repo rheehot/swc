8d7052ac5bda3a9db60ec13d1398f00abb796e27
강동윤 <kdy1@outlook.kr>
2019-06-24 19:56:43 +0900

    Handle alias

--- typescript/checker/src/analyzer/expr.rs
+++ typescript/checker/src/analyzer/expr.rs
@@ -1013,6 +1013,11 @@ impl Analyzer<'_, '_> {
                                     Type::Interface(..) | Type::Class(..) => {
                                         return Ok(ty.static_cast())
                                     }
+
+                                    Type::Alias(ref a) => {
+                                        // Expand type alias
+                                        return Ok(a.ty.static_cast());
+                                    }
                                     _ => {}
                                 }
                             }
