6960edba5e064276a7d7d3e24740dcf44cf3929f
강동윤 <kdy1@outlook.kr>
2019-07-03 20:50:21 +0900

    [WIP] Test only `types/tuple/wideningTuples`

--- typescript/checker/tests/tests.rs
+++ typescript/checker/tests/tests.rs
@@ -121,7 +121,7 @@ fn add_tests(tests: &mut Vec<TestDescAndFn>, mode: Mode) -> Result<(), io::Error
             || input.contains("@filename")
             || input.contains("@Filename")
             || input.contains("@module")
-            || (mode == Mode::Conformance && !file_name.contains(""));
+            || (mode == Mode::Conformance && !file_name.contains("types/tuple/wideningTuples"));
 
         let dir = dir.clone();
         let name = format!("tsc::{}::{}", test_kind, file_name);
