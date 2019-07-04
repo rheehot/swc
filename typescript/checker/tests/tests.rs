7e6b26447388377d96dad02c07bc5915d9f17711
강동윤 <kdy1@outlook.kr>
2019-07-04 17:41:30 +0900

    Run witness test (again)

--- typescript/checker/tests/tests.rs
+++ typescript/checker/tests/tests.rs
@@ -121,7 +121,7 @@ fn add_tests(tests: &mut Vec<TestDescAndFn>, mode: Mode) -> Result<(), io::Error
             || input.contains("@filename")
             || input.contains("@Filename")
             || input.contains("@module")
-            || (mode == Mode::Conformance && !file_name.contains("types/tuple/wideningTuples"));
+            || (mode == Mode::Conformance && !file_name.contains("types/witness"));
 
         let dir = dir.clone();
         let name = format!("tsc::{}::{}", test_kind, file_name);
