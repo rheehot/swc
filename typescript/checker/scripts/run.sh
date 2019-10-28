9a398d7b759165bbccecd9ff53044ec1398d78dc
강동윤 <kdy1997.dev@gmail.com>
2019-10-28 12:00:58 +0900

    Add a crazy script to prevent regressions

---
+++ typescript/checker/scripts/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -eu
+
+#
+# Usage: ./scripts/run.sh "types/never"
+#
+
+cargo fmt
+cargo check
+
+
+# We are done, and I don't want regression.
+TEST="constEnums" cargo test --test tests -- conformance
+
+# We are developing
+TEST="$1" cargo test --test tests -- conformance
+TEST="" cargo test --test tests -- conformance
