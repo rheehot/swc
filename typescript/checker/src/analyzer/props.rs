ee7f0804c9844e3440de81c9080f836d4e7a08f2
강동윤 <kdy1997.dev@gmail.com>
2019-11-02 23:39:24 +0900

    Validate getters in class

--- typescript/checker/src/analyzer/props.rs
+++ typescript/checker/src/analyzer/props.rs
@@ -3,51 +3,74 @@ use crate::errors::Error;
 use swc_common::{Spanned, Visit, VisitWith};
 use swc_ecma_ast::*;
 
-macro_rules! visit_method_like {
-    ($a:expr, $node:expr) => {{
-        let entry = $a.with_child(ScopeKind::Fn, Default::default(), |child| {
-            child.return_type_span = $node.span();
+impl Visit<GetterProp> for Analyzer<'_, '_> {
+    fn visit(&mut self, n: &GetterProp) {
+        let entry = self.with_child(ScopeKind::Fn, Default::default(), |child| {
+            child.return_type_span = n.span();
 
             child
                 .inferred_return_types
                 .get_mut()
-                .insert($node.span(), Default::default());
+                .insert(n.span(), Default::default());
 
-            $node.visit_children(child);
+            n.visit_children(child);
 
             child
                 .inferred_return_types
                 .get_mut()
-                .remove_entry(&$node.span())
+                .remove_entry(&n.span())
                 .unwrap_or_default()
         });
 
         if entry.1.is_empty() {
             // getter property must have return statements.
-            $a.info.errors.push(Error::GetterPropWithoutReturn {
-                span: $node.key.span(),
-            });
+            self.info
+                .errors
+                .push(Error::GetterPropWithoutReturn { span: n.key.span() });
         }
 
-        *$a.inferred_return_types
+        *self
+            .inferred_return_types
             .get_mut()
-            .entry($node.span())
+            .entry(n.span())
             .or_default() = entry.1;
-    }};
-}
-
-impl Visit<GetterProp> for Analyzer<'_, '_> {
-    fn visit(&mut self, prop: &GetterProp) {
-        visit_method_like!(self, prop);
     }
 }
 
-// impl Visit<ClassMethod> for Analyzer<'_, '_> {
-//     fn visit(&mut self, m: &ClassMethod) {
-//         if m.kind == MethodKind::Getter {
-//             visit_method_like!(self, m);
-//         } else {
-//             m.visit_children(self)
-//         }
-//     }
-// }
+impl Visit<ClassMethod> for Analyzer<'_, '_> {
+    fn visit(&mut self, n: &ClassMethod) {
+        if n.kind == MethodKind::Getter {
+            let entry = self.with_child(ScopeKind::Fn, Default::default(), |child| {
+                child.return_type_span = n.span();
+
+                child
+                    .inferred_return_types
+                    .get_mut()
+                    .insert(n.span(), Default::default());
+
+                n.function.visit_children(child);
+
+                child
+                    .inferred_return_types
+                    .get_mut()
+                    .remove_entry(&n.span())
+                    .unwrap_or_default()
+            });
+
+            if entry.1.is_empty() {
+                // getter property must have return statements.
+                self.info
+                    .errors
+                    .push(Error::GetterPropWithoutReturn { span: n.key.span() });
+            }
+
+            *self
+                .inferred_return_types
+                .get_mut()
+                .entry(n.span())
+                .or_default() = entry.1;
+        } else {
+            n.visit_children(self)
+        }
+    }
+}
