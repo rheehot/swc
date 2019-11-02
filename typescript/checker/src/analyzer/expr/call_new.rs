f7e37573b2bdb9a1172698b3c2b048ee12d4c5c1
강동윤 <kdy1997.dev@gmail.com>
2019-11-02 20:28:24 +0900

    More for TS2347

--- typescript/checker/src/analyzer/expr/call_new.rs
+++ typescript/checker/src/analyzer/expr/call_new.rs
@@ -1,6 +1,6 @@
 use crate::{analyzer::Analyzer, errors::Error, ty::Type};
 use swc_common::{Visit, VisitWith};
-use swc_ecma_ast::{NewExpr, TsKeywordType, TsKeywordTypeKind};
+use swc_ecma_ast::*;
 
 impl Visit<NewExpr> for Analyzer<'_, '_> {
     fn visit(&mut self, e: &NewExpr) {
@@ -14,6 +14,29 @@ impl Visit<NewExpr> for Analyzer<'_, '_> {
                     ..
                 }) if e.type_args.is_some() => Err(Error::TS2347 { span: e.span })?,
                 _ => {}
+            }
+        };
+
+        if let Err(err) = res {
+            self.info.errors.push(err);
+        }
+    }
+}
+
+impl Visit<CallExpr> for Analyzer<'_, '_> {
+    fn visit(&mut self, e: &CallExpr) {
+        e.visit_children(self);
+
+        let res: Result<(), Error> = try {
+            if let ExprOrSuper::Expr(ref callee) = e.callee {
+                let callee_ty = self.type_of(&callee)?;
+                match *callee_ty.normalize() {
+                    Type::Keyword(TsKeywordType {
+                        kind: TsKeywordTypeKind::TsAnyKeyword,
+                        ..
+                    }) if e.type_args.is_some() => Err(Error::TS2347 { span: e.span })?,
+                    _ => {}
+                }
             }
         };
 
