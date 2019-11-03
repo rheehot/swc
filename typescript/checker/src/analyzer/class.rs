09db95fc5b971fdeb4b75d2972373cb749235c59
강동윤 <kdy1997.dev@gmail.com>
2019-11-03 21:29:14 +0900

    Split class module

---
+++ typescript/checker/src/analyzer/class.rs
@@ -0,0 +1,88 @@
+use super::{scope::ScopeKind, Analyzer};
+use crate::ty::Type;
+use swc_common::{Spanned, Visit, VisitWith};
+use swc_ecma_ast::*;
+
+impl Visit<Class> for Analyzer<'_, '_> {
+    fn visit(&mut self, c: &Class) {
+        self.validate_parent_interfaces(&c.implements);
+
+        c.visit_children(self);
+    }
+}
+
+impl Visit<ClassExpr> for Analyzer<'_, '_> {
+    fn visit(&mut self, c: &ClassExpr) {
+        let ty = match self.validate_type_of_class(c.ident.clone().map(|v| v.sym), &c.class) {
+            Ok(ty) => ty,
+            Err(err) => {
+                self.info.errors.push(err);
+                Type::any(c.span()).into()
+            }
+        };
+
+        self.scope.this = Some(ty.clone());
+
+        self.with_child(ScopeKind::Block, Default::default(), |analyzer| {
+            if let Some(ref i) = c.ident {
+                analyzer.scope.register_type(i.sym.clone(), ty.clone());
+
+                match analyzer.scope.declare_var(
+                    ty.span(),
+                    VarDeclKind::Var,
+                    i.sym.clone(),
+                    Some(ty),
+                    // initialized = true
+                    true,
+                    // declare Class does not allow multiple declarations.
+                    false,
+                ) {
+                    Ok(()) => {}
+                    Err(err) => {
+                        analyzer.info.errors.push(err);
+                    }
+                }
+            }
+
+            c.visit_children(analyzer);
+        });
+
+        self.scope.this = None;
+    }
+}
+
+impl Visit<ClassDecl> for Analyzer<'_, '_> {
+    fn visit(&mut self, c: &ClassDecl) {
+        let ty = match self.validate_type_of_class(Some(c.ident.sym.clone()), &c.class) {
+            Ok(ty) => ty,
+            Err(err) => {
+                self.info.errors.push(err);
+                Type::any(c.span()).into()
+            }
+        };
+
+        self.scope.this = Some(ty.clone());
+
+        self.scope.register_type(c.ident.sym.clone(), ty.clone());
+
+        match self.scope.declare_var(
+            ty.span(),
+            VarDeclKind::Var,
+            c.ident.sym.clone(),
+            Some(ty),
+            // initialized = true
+            true,
+            // declare Class does not allow multiple declarations.
+            false,
+        ) {
+            Ok(()) => {}
+            Err(err) => {
+                self.info.errors.push(err);
+            }
+        }
+
+        c.visit_children(self);
+
+        self.scope.this = None;
+    }
+}
--- typescript/checker/src/analyzer/mod.rs
+++ typescript/checker/src/analyzer/mod.rs
@@ -21,6 +21,7 @@ use swc_common::{Span, Spanned, Visit, VisitWith, DUMMY_SP};
 use swc_ecma_ast::*;
 
 mod assign;
+mod class;
 mod control_flow;
 mod enums;
 pub mod export;
@@ -318,88 +319,6 @@ pub struct ImportInfo {
 pub struct Specifier {
     pub local: (JsWord, Span),
     pub export: (JsWord, Span),
-}
-
-impl Visit<ClassExpr> for Analyzer<'_, '_> {
-    fn visit(&mut self, c: &ClassExpr) {
-        let ty = match self.validate_type_of_class(c.ident.clone().map(|v| v.sym), &c.class) {
-            Ok(ty) => ty,
-            Err(err) => {
-                self.info.errors.push(err);
-                Type::any(c.span()).into()
-            }
-        };
-
-        self.scope.this = Some(ty.clone());
-
-        self.with_child(ScopeKind::Block, Default::default(), |analyzer| {
-            if let Some(ref i) = c.ident {
-                analyzer.scope.register_type(i.sym.clone(), ty.clone());
-
-                match analyzer.scope.declare_var(
-                    ty.span(),
-                    VarDeclKind::Var,
-                    i.sym.clone(),
-                    Some(ty),
-                    // initialized = true
-                    true,
-                    // declare Class does not allow multiple declarations.
-                    false,
-                ) {
-                    Ok(()) => {}
-                    Err(err) => {
-                        analyzer.info.errors.push(err);
-                    }
-                }
-            }
-
-            c.visit_children(analyzer);
-        });
-
-        self.scope.this = None;
-    }
-}
-
-impl Visit<Class> for Analyzer<'_, '_> {
-    fn visit(&mut self, c: &Class) {
-        self.validate_parent_interfaces(&c.implements);
-    }
-}
-
-impl Visit<ClassDecl> for Analyzer<'_, '_> {
-    fn visit(&mut self, c: &ClassDecl) {
-        let ty = match self.validate_type_of_class(Some(c.ident.sym.clone()), &c.class) {
-            Ok(ty) => ty,
-            Err(err) => {
-                self.info.errors.push(err);
-                Type::any(c.span()).into()
-            }
-        };
-
-        self.scope.this = Some(ty.clone());
-
-        self.scope.register_type(c.ident.sym.clone(), ty.clone());
-
-        match self.scope.declare_var(
-            ty.span(),
-            VarDeclKind::Var,
-            c.ident.sym.clone(),
-            Some(ty),
-            // initialized = true
-            true,
-            // declare Class does not allow multiple declarations.
-            false,
-        ) {
-            Ok(()) => {}
-            Err(err) => {
-                self.info.errors.push(err);
-            }
-        }
-
-        c.visit_children(self);
-
-        self.scope.this = None;
-    }
 }
 
 impl Analyzer<'_, '_> {
