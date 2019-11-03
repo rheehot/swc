b0f750b185c1d1fb827d60fb41fca99cc79a5863
강동윤 <kdy1997.dev@gmail.com>
2019-11-03 21:25:21 +0900

    Remove logging

--- typescript/checker/src/analyzer/export.rs
+++ typescript/checker/src/analyzer/export.rs
@@ -96,9 +96,7 @@ impl Analyzer<'_, '_> {
 
 impl Visit<ExportDecl> for Analyzer<'_, '_> {
     fn visit(&mut self, export: &ExportDecl) {
-        println!("FOO!");
         export.visit_children(self);
-        println!("BAR!");
 
         match export.decl {
             Decl::Fn(ref f) => self.export(f.span(), f.ident.sym.clone(), None),
