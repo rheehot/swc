85296eccca0edc060aa67e612cc197b0a64dcb67
강동윤 <kdy1997.dev@gmail.com>
2019-11-06 14:10:33 +0900

    Handle `function foo(...a, b)` gracefully

--- ecmascript/parser/src/parser/pat.rs
+++ ecmascript/parser/src/parser/pat.rs
@@ -335,7 +335,6 @@ impl<'a, I: Tokens> Parser<'a, I> {
 
             if eat!("...") {
                 dot3_token = span!(start);
-
                 let mut pat = self.parse_binding_pat_or_ident()?;
 
                 if eat!('=') {
@@ -350,8 +349,6 @@ impl<'a, I: Tokens> Parser<'a, I> {
                     .into();
                 }
 
-                let type_ann = if self.input.syntax().typescript() && is!(':') {
-                    let cur_pos = cur_pos!();
                     let ty = self.parse_ts_type_ann(/* eat_colon */ true, cur_pos)?;
                     Some(ty)
                 } else {
