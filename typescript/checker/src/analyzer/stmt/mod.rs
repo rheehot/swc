696399453fe1ce6ae3d08a7e18acbd35ad0356ed
강동윤 <kdy1997.dev@gmail.com>
2019-11-07 21:21:40 +0900

    (+1279) TS1123

--- typescript/checker/src/analyzer/stmt/mod.rs
+++ typescript/checker/src/analyzer/stmt/mod.rs
@@ -9,7 +9,12 @@ macro_rules! impl_for {
                 n.visit_children(self);
 
                 self.check_lhs_of_for_loop(&n.left);
-                self.check_rhs_of_for_loop(&n.right);
+                if match n.left {
+                    VarDeclOrPat::VarDecl(VarDecl { ref decls, .. }) => !decls.is_empty(),
+                    _ => true,
+                } {
+                    self.check_rhs_of_for_loop(&n.right);
+                }
             }
         }
     };
