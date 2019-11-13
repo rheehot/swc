1806a785d3b26b28957ae7c35b28a171ee251342
강동윤 <kdy1997.dev@gmail.com>
2019-11-13 12:26:22 +0900

    success.py: Fix

--- typescript/checker/scripts/success.py
+++ typescript/checker/scripts/success.py
@@ -5,19 +5,28 @@
 
 def read_successes():
     with open('./tests/success.txt', 'r') as f:
-        return list(set(f.readlines()))
+        return sorted(list(set(f.readlines())))
+
+def read_done():
+    with open('./tests/done.txt', 'r') as f:
+        return sorted(list(set(f.readlines())))
 
 
 def main():
-    new_suceesses = list()
     successed = read_successes()
-    with open('tests/done.txt', 'r') as done:
-        for d in done.readlines():
-            for s in successed:
-                new_suceesses.append(s)
-    with open('./tests/success.txt', 'w') as f:
-        f.write(''.join(new_suceesses))
-
+    done = read_done()
+    print('loaded success.txt and done.txt')
+    new_successes = []
+    for s in successed:
+        matched = False
+        for d in done:
+            if s.strip('\n').startswith(d.strip('\n')):
+                matched = True
+                break
+        if not matched:
+            new_successes.append(s)
+    with open('tests/success.txt', 'w') as f:
+        f.write(''.join(new_successes))
 
 if __name__ == '__main__':
     main()
