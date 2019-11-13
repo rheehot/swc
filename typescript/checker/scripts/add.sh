ace950f6a4e5f9d9030945ead2adccfd07559941
강동윤 <kdy1997.dev@gmail.com>
2019-11-13 13:25:21 +0900

    add.sh: run success.py

--- typescript/checker/scripts/add.sh
+++ typescript/checker/scripts/add.sh
@@ -8,10 +8,12 @@ set -eu
 cargo fmt
 #cargo check
 
+bash ./scripts/sort.sh
+./scripts/success.py
 
 # We are developing
 TEST="$1" cargo test --test tests -- conformance
 
 echo "$1" >> ./tests/done.txt
-
 bash ./scripts/sort.sh
+./scripts/success.py
