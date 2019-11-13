4d3a529f020eba1ef3916667c76a11327555cf18
강동윤 <kdy1997.dev@gmail.com>
2019-11-13 12:01:56 +0900

    success.py

---
+++ typescript/checker/scripts/success.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# This file remove golden (done) tests from ./tests/success.txt
+
+
+def read_successes():
+    with open('./tests/success.txt', 'r') as f:
+        return f.readlines()
+
+
+def main():
+    new_suceesses = list()
+    successed = read_successes()
+    with open('tests/done.txt', 'r') as done:
+        for d in done.readlines():
+            for s in successed:
+                new_suceesses.append(s)
+    with open('./tests/success.txt', 'w') as f:
+        f.write(''.join(new_suceesses))
+
+
+if __name__ == '__main__':
+    main()
