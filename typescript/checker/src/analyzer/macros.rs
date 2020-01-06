db4ed1140624b97aaddcda5532c70c87c72bbb89
강동윤 <kdy1997.dev@gmail.com>
2020-01-06 13:06:19 +0900

    disable prevent!

--- typescript/checker/src/analyzer/macros.rs
+++ typescript/checker/src/analyzer/macros.rs
@@ -1,20 +1,20 @@
 macro_rules! prevent {
     ($T:ty) => {
-        /// # Note
-        ///
-        /// This implementation **always panics**, and a dedicated method should be
-        /// called instead of visit<T>
-        impl ::swc_common::Visit<$T> for Analyzer<'_, '_> {
-            fn visit(&mut self, n: &$T) {
-                unreachable!(
-                    "Visit<{}> should be not called and {} should be handled by a dedicated \
-             method\n{:?}",
-                    stringify!($T),
-                    stringify!($T),
-                    n
-                );
-            }
-        }
+        ///// # Note
+        /////
+        ///// This implementation **always panics**, and a dedicated method should be
+        ///// called instead of visit<T>
+        //impl ::swc_common::Visit<$T> for Analyzer<'_, '_> {
+        //    fn visit(&mut self, n: &$T) {
+        //        unreachable!(
+        //            "Visit<{}> should be not called and {} should be handled by a
+        // dedicated \     method\n{:?}",
+        //            stringify!($T),
+        //            stringify!($T),
+        //            n
+        //        );
+        //    }
+        //}
     };
 }
 
