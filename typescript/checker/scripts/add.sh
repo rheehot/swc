875a6cdb16af4c7c34826ef26c9f46b3d134c363
강동윤 <kdy1997.dev@gmail.com>
2020-01-07 20:31:40 +0900

    Improve add.sh

--- typescript/checker/scripts/add.sh
+++ typescript/checker/scripts/add.sh
@@ -8,8 +8,8 @@ set -eu
 cargo fmt
 #cargo check
 
-if [[ $1 != */ ]] && [[ $1 != *\.ts ]]; then
-  echo 'Wrong invokation. Argument must end with "/" or ".ts"'
+if [[ $1 != */ ]] && [[ $1 != *\.ts ]] && [[ ! -z $1 ]]; then
+  echo 'Wrong invokation. Argument must empty or end with "/" or ".ts"'
   exit 1
 fi
 
@@ -18,7 +18,11 @@ bash ./scripts/sort.sh
 
 # We are developing
 export TEST="$1"
-( set -o pipefail; cargo test --test tests -- conformance | grep --color -E 'swc_ecma_ast|swc_ts_checker|$' )
+if [[ -z $1 ]]; then
+  cargo test --test tests -- conformance
+else
+  ( set -o pipefail; cargo test --test tests -- conformance | grep --color -E 'swc_ecma_ast|swc_ts_checker|$' )
+fi
 
 echo "$1" >> ./tests/done.txt
 bash ./scripts/sort.sh
