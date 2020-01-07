e6a42b84219c90c7b0e28af639698aeb333c5340
강동윤 <kdy1997.dev@gmail.com>
2020-01-07 19:14:57 +0900

    Move code related to function

--- typescript/checker/src/analyzer/function.rs
+++ typescript/checker/src/analyzer/function.rs
@@ -1,11 +1,115 @@
 use super::Analyzer;
 use crate::{
+    analyzer::{pat::PatMode, Ctx, ScopeKind},
+    errors::Error,
     ty,
-    ty::{QueryType, Tuple, Type},
-    validator::ValidateWith,
+    ty::{ClassInstance, QueryType, Tuple, Type},
+    validator::{Validate, ValidateWith},
+    ValidationResult,
 };
 use swc_common::{Spanned, Visit};
 use swc_ecma_ast::*;
+
+impl Validate<Function> for Analyzer<'_, '_> {
+    type Output = ValidationResult<ty::Function>;
+
+    fn validate(&mut self, f: &Function) -> Self::Output {
+        self.with_child(ScopeKind::Fn, Default::default(), |child| {
+            let mut errors = vec![];
+
+            {
+                // Validate params
+                // TODO: Move this to parser
+                let mut has_optional = false;
+                for p in &f.params {
+                    if has_optional {
+                        child.info.errors.push(Error::TS1016 { span: p.span() });
+                    }
+
+                    match *p {
+                        Pat::Ident(Ident { optional, .. }) => {
+                            if optional {
+                                has_optional = true;
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+            }
+
+            let type_params = try_opt!(f.type_params.validate_with(child));
+
+            let params = {
+                let ctx = Ctx {
+                    pat_mode: PatMode::Decl,
+                    allow_ref_declaring: false,
+                    ..child.ctx
+                };
+                f.params.validate_with(&mut *child.with_ctx(ctx))?
+            };
+
+            let declared_ret_ty = try_opt!(f.return_type.validate_with(child)).map(|ret_ty| {
+                let span = ret_ty.span();
+                match ret_ty {
+                    Type::Class(cls) => Type::ClassInstance(ClassInstance {
+                        span,
+                        cls,
+                        type_args: None,
+                    }),
+                    ty => ty,
+                }
+            });
+
+            let inferred_return_type = try_opt!(f
+                .body
+                .as_ref()
+                .map(|body| child.visit_stmts_for_return(&body.stmts)));
+            let inferred_return_type = match inferred_return_type {
+                Some(Some(inferred_return_type)) => {
+                    if let Some(ref declared) = declared_ret_ty {
+                        let span = inferred_return_type.span();
+
+                        child.assign(&declared, &inferred_return_type, span)?;
+                    }
+
+                    inferred_return_type
+                }
+                Some(None) => {
+                    let mut span = f.span;
+
+                    if let Some(ref declared) = declared_ret_ty {
+                        span = declared.span();
+
+                        match *declared.normalize() {
+                            Type::Keyword(TsKeywordType {
+                                kind: TsKeywordTypeKind::TsAnyKeyword,
+                                ..
+                            })
+                            | Type::Keyword(TsKeywordType {
+                                kind: TsKeywordTypeKind::TsVoidKeyword,
+                                ..
+                            }) => {}
+                            _ => errors.push(Error::ReturnRequired { span }),
+                        }
+                    }
+
+                    Type::any(span)
+                }
+                None => Type::any(f.span),
+            };
+
+            child.info.errors.extend(errors);
+
+            Ok(ty::Function {
+                span: f.span,
+                params,
+                type_params,
+                ret_ty: box declared_ret_ty.unwrap_or_else(|| inferred_return_type),
+            }
+            .into())
+        })
+    }
+}
 
 impl Analyzer<'_, '_> {
     /// TODO: Handle recursive funciton
