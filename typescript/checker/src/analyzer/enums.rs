dbf6750e402b3a4002cb3eb2e901453ba2313aba
강동윤 <kdy1997.dev@gmail.com>
2020-01-09 23:42:33 +0900

    Use macro

--- typescript/checker/src/analyzer/enums.rs
+++ typescript/checker/src/analyzer/enums.rs
@@ -2,38 +2,68 @@ use super::Analyzer;
 use crate::{
     analyzer::util::ResultExt,
     errors::Error,
-    ty::{Enum, Type},
+    ty::{Enum, EnumMember, Type},
     validator::Validate,
+    ValidationResult,
 };
+use macros::validator;
 use swc_common::{Spanned, Visit, VisitWith};
 use swc_ecma_ast::*;
 
-/// Handles enum.
-impl Visit<TsEnumDecl> for Analyzer<'_, '_> {
-    fn visit(&mut self, e: &TsEnumDecl) {
-        let span = e.span();
+/// We don't visit enum variants to allow
+///
+///        const enum E {
+///            a = 10,
+///            b = a,
+///            c = (a+1),
+///            e,
+///            d = ~e,
+///            f = a << 2 >> 1,
+///            g = a << 2 >>> 1,
+///            h = a | b
+///        }
+#[validator]
+impl Validate<TsEnumDecl> for Analyzer<'_, '_> {
+    type Output = ValidationResult<Enum>;
+    #[inline(never)]
+    fn validate(&mut self, e: &TsEnumDecl) -> Self::Output {
+        let ty: Result<_, _> = try {
+            let members = e
+                .members
+                .iter()
+                .enumerate()
+                .map(|(i, m)| -> Result<_, Error> {
+                    Ok(EnumMember {
+                        id: m.id.clone(),
+                        val: compute(&e, i, m.init.as_ref().map(|v| &**v))?,
+                        span: m.span,
+                    })
+                })
+                .collect::<Result<Vec<_>, _>>()?;
 
-        // We don't visit enum variants to allow
-        //
-        //        const enum E {
-        //            a = 10,
-        //            b = a,
-        //            c = (a+1),
-        //            e,
-        //            d = ~e,
-        //            f = a << 2 >> 1,
-        //            g = a << 2 >>> 1,
-        //            h = a | b
-        //        }
+            Enum {
+                span: e.span,
+                has_num: members.iter().any(|m| match m.val {
+                    TsLit::Number(..) => true,
+                    _ => false,
+                }),
+                has_str: members.iter().any(|m| match m.val {
+                    TsLit::Str(..) => true,
+                    _ => false,
+                }),
+                declare: e.declare,
+                is_const: e.is_const,
+                id: e.id.clone(),
+                members,
+            }
+        };
 
-        //        e.visit_children(self);
-
-        let ty = self.validate(e);
+        let span = e.span;
 
         self.register_type(
             e.id.sym.clone(),
             match ty {
-                Ok(ty) => ty.into(),
+                Ok(ref ty) => ty.clone().into(),
                 Err(..) => Type::any(span),
             },
         )
@@ -58,7 +88,89 @@ impl Visit<TsEnumDecl> for Analyzer<'_, '_> {
         } else {
             e.members.visit_children(self);
         }
+
+        ty
     }
+}
+
+/// Called only for enums.
+fn compute(e: &TsEnumDecl, i: usize, expr: Option<&Expr>) -> Result<TsLit, Error> {
+    fn arithmetic_opt(e: &TsEnumDecl, i: usize, expr: Option<&Expr>) -> Result<f64, Error> {
+        if let Some(ref expr) = expr {
+            return arithmetic(e, expr);
+        }
+
+        return Ok(i as f64);
+    }
+
+    fn arithmetic(e: &TsEnumDecl, expr: &Expr) -> Result<f64, Error> {
+        Ok(match *expr {
+            Expr::Lit(ref lit) => match *lit {
+                Lit::Num(ref v) => v.value,
+                _ => unreachable!("arithmetic({:?})", lit),
+            },
+            Expr::Bin(ref bin) => arithmetic_bin(e, &bin)?,
+            Expr::Paren(ref paren) => return arithmetic(e, &paren.expr),
+
+            Expr::Ident(ref id) => {
+                //
+                for (i, m) in e.members.iter().enumerate() {
+                    match m.id {
+                        TsEnumMemberId::Str(Str { value: ref sym, .. })
+                        | TsEnumMemberId::Ident(Ident { ref sym, .. }) => {
+                            if *sym == id.sym {
+                                return arithmetic_opt(e, i, m.init.as_ref().map(|v| &**v));
+                            }
+                        }
+                    }
+                }
+
+                return Err(Error::InvalidEnumInit { span: expr.span() });
+            }
+            Expr::Unary(..) => unimplemented!("compute(unary, {:?})", expr),
+            _ => return Err(Error::InvalidEnumInit { span: expr.span() }),
+        })
+    }
+
+    fn arithmetic_bin(e: &TsEnumDecl, expr: &BinExpr) -> Result<f64, Error> {
+        let l = arithmetic(e, &expr.left)?;
+        let r = arithmetic(e, &expr.right)?;
+
+        Ok(match expr.op {
+            op!(bin, "+") => l + r,
+            op!(bin, "-") => l - r,
+            op!("*") => l * r,
+            op!("/") => l / r,
+
+            // TODO
+            op!("&") => ((l.round() as i64) & (r.round() as i64)) as _,
+            op!("|") => ((l.round() as i64) | (r.round() as i64)) as _,
+            op!("^") => ((l.round() as i64) ^ (r.round() as i64)) as _,
+
+            op!("<<") => ((l.round() as i64) << (r.round() as i64)) as _,
+            op!(">>") => ((l.round() as i64) >> (r.round() as i64)) as _,
+            _ => unimplemented!("arithmetic_bin({:?})", expr.op),
+        })
+    }
+
+    fn try_str(e: &Expr) -> Result<Str, ()> {
+        match *e {
+            Expr::Lit(Lit::Str(ref s)) => return Ok(s.clone()),
+            _ => Err(()),
+        }
+    }
+
+    if let Some(ref expr) = expr {
+        if let Ok(s) = try_str(&expr) {
+            return Ok(s.into());
+        }
+    }
+
+    Ok(Number {
+        span: expr.span(),
+        value: arithmetic_opt(e, i, expr)?,
+    }
+    .into())
 }
 
 impl Analyzer<'_, '_> {
