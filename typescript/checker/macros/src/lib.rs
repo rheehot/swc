9c6c00596a35765c0f9cdcb9c7865af7d2a7aa59
강동윤 <kdy1997.dev@gmail.com>
2020-01-09 22:05:48 +0900

    private

--- typescript/checker/macros/src/lib.rs
+++ typescript/checker/macros/src/lib.rs
@@ -50,65 +50,65 @@ pub fn validator_method(
     _: proc_macro::TokenStream,
     item: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
+    fn expand_validator_method(i: ImplItemMethod) -> ImplItemMethod {
+        let should_return = match i.sig.output {
+            ReturnType::Default => false,
+            _ => true,
+        };
+
+        let try_block = ExprTryBlock {
+            attrs: Default::default(),
+            try_token: call_site(),
+            block: i.block,
+        };
+
+        let block = if should_return {
+            Quote::new_call_site()
+                .quote_with(smart_quote!(
+                    Vars {
+                        try_block: &try_block
+                    },
+                    {
+                        {
+                            let res: Result<_, Error> = try_block;
+
+                            match res {
+                                Ok(v) => Ok(v),
+                                Err(err) => {
+                                    self.info.errors.push(err);
+                                    Err(())
+                                }
+                            }
+                        }
+                    }
+                ))
+                .parse()
+        } else {
+            Quote::new_call_site()
+                .quote_with(smart_quote!(
+                    Vars {
+                        try_block: &try_block
+                    },
+                    {
+                        {
+                            let res: Result<_, Error> = try_block;
+
+                            match res {
+                                Err(err) => {
+                                    self.info.errors.push(err);
+                                }
+                                _ => {}
+                            }
+                        }
+                    }
+                ))
+                .parse()
+        };
+
+        ImplItemMethod { block, ..i }
+    }
+
     let item = syn::parse(item).expect("failed to parse input as an item");
-    let item = expand_validator(item);
+    let item = expand_validator_method(item);
     print("validator", item.dump())
-}
-
-fn expand_validator(i: ImplItemMethod) -> ImplItemMethod {
-    let should_return = match i.sig.output {
-        ReturnType::Default => false,
-        _ => true,
-    };
-
-    let try_block = ExprTryBlock {
-        attrs: Default::default(),
-        try_token: call_site(),
-        block: i.block,
-    };
-
-    let block = if should_return {
-        Quote::new_call_site()
-            .quote_with(smart_quote!(
-                Vars {
-                    try_block: &try_block
-                },
-                {
-                    {
-                        let res: Result<_, Error> = try_block;
-
-                        match res {
-                            Ok(v) => Ok(v),
-                            Err(err) => {
-                                self.info.errors.push(err);
-                                Err(())
-                            }
-                        }
-                    }
-                }
-            ))
-            .parse()
-    } else {
-        Quote::new_call_site()
-            .quote_with(smart_quote!(
-                Vars {
-                    try_block: &try_block
-                },
-                {
-                    {
-                        let res: Result<_, Error> = try_block;
-
-                        match res {
-                            Err(err) => {
-                                self.info.errors.push(err);
-                            }
-                            _ => {}
-                        }
-                    }
-                }
-            ))
-            .parse()
-    };
-
-    ImplItemMethod { block, ..i }
 }
