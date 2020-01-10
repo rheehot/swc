c201d27b251f4c18b5c7e18b19232f70b0dbe8e8
강동윤 <kdy1997.dev@gmail.com>
2020-01-10 11:41:08 +0900

    Improve handler of for in/of statements

--- typescript/checker/src/analyzer/stmt/loops.rs
+++ typescript/checker/src/analyzer/stmt/loops.rs
@@ -1,6 +1,6 @@
 use super::super::Analyzer;
 use crate::{
-    analyzer::expr::TypeOfMode,
+    analyzer::{expr::TypeOfMode, ScopeKind},
     errors::Error,
     ty::{Array, Type},
     validator::Validate,
@@ -12,9 +12,11 @@ use swc_ecma_ast::*;
 impl Analyzer<'_, '_> {
     #[validator_method]
     fn check_lhs_of_for_loop(&mut self, e: &VarDeclOrPat) {
-        // Check iterable
         match *e {
-            VarDeclOrPat::VarDecl(..) => {}
+            VarDeclOrPat::VarDecl(ref v) => {
+                // Store variables
+                v.visit_with(self);
+            }
             VarDeclOrPat::Pat(ref pat) => match *pat {
                 Pat::Expr(ref e) => {
                     self.validate_expr(&e, TypeOfMode::LValue, None)?;
@@ -67,28 +69,32 @@ impl Analyzer<'_, '_> {
 
 impl Visit<ForInStmt> for Analyzer<'_, '_> {
     fn visit(&mut self, s: &ForInStmt) {
-        self.check_lhs_of_for_loop(&s.left);
-        if match s.left {
-            VarDeclOrPat::VarDecl(VarDecl { ref decls, .. }) => !decls.is_empty(),
-            _ => true,
-        } {
-            self.check_rhs_of_for_loop(&s.right);
-        }
+        self.with_child(ScopeKind::Flow, Default::default(), |child| {
+            self.check_lhs_of_for_loop(&s.left);
+            if match s.left {
+                VarDeclOrPat::VarDecl(VarDecl { ref decls, .. }) => !decls.is_empty(),
+                _ => true,
+            } {
+                self.check_rhs_of_for_loop(&s.right);
+            }
 
-        self.validate_for_loop(s.span, &s.left, &s.right);
+            self.validate_for_loop(s.span, &s.left, &s.right);
+        })
     }
 }
 
 impl Visit<ForOfStmt> for Analyzer<'_, '_> {
     fn visit(&mut self, s: &ForOfStmt) {
-        self.check_lhs_of_for_loop(&s.left);
-        if match s.left {
-            VarDeclOrPat::VarDecl(VarDecl { ref decls, .. }) => !decls.is_empty(),
-            _ => true,
-        } {
-            self.check_rhs_of_for_loop(&s.right);
-        }
+        self.with_child(ScopeKind::Flow, Default::default(), |child| {
+            child.check_lhs_of_for_loop(&s.left);
+            if match s.left {
+                VarDeclOrPat::VarDecl(VarDecl { ref decls, .. }) => !decls.is_empty(),
+                _ => true,
+            } {
+                child.check_rhs_of_for_loop(&s.right);
+            }
 
-        self.validate_for_loop(s.span, &s.left, &s.right);
+            child.validate_for_loop(s.span, &s.left, &s.right);
+        })
     }
 }
