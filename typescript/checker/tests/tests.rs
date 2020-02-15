efce2de7f09764e1a5ee10fa22e1e2dd85094200
강동윤 <kdy1997.dev@gmail.com>
2020-02-15 19:20:25 +0900

    ignore.txt

--- typescript/checker/tests/tests.rs
+++ typescript/checker/tests/tests.rs
@@ -141,7 +141,7 @@ static DONE: Lazy<Vec<&'static str>> = Lazy::new(|| {
 
 /// We are done and I don't want regression.
 static IGNORED: Lazy<Vec<&'static str>> = Lazy::new(|| {
-    let mut f = File::open(&format!("{}/tests/ignored.txt", env!("CARGO_MANIFEST_DIR")))
+    let mut f = File::open(&format!("{}/tests/ignore.txt", env!("CARGO_MANIFEST_DIR")))
         .expect("failed to open file");
     let mut s = String::new();
     f.read_to_string(&mut s).expect("failed to read file");
