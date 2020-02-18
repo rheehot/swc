fe8c4810efe607c4d8536c33647c4e50eede0308
강동윤 <kdy1997.dev@gmail.com>
2020-02-18 17:19:21 +0900

    dts generator: FnDecl

--- typescript/dts/src/lib.rs
+++ typescript/dts/src/lib.rs
@@ -3,11 +3,13 @@
 #![feature(specialization)]
 
 use std::collections::hash_map::Entry;
+use swc_atoms::JsWord;
 use swc_common::{Fold, FoldWith};
 use swc_ecma_ast::*;
 use swc_ts_checker::{ty::Type, ModuleTypeInfo};
 
 pub fn generate_dts(module: Module, info: ModuleTypeInfo) -> Module {
+    println!("Info: {:?}", info);
     module.fold_with(&mut TypeResolver { info })
 }
 
@@ -16,7 +18,21 @@ struct TypeResolver {
     info: ModuleTypeInfo,
 }
 
-impl TypeResolver {}
+impl TypeResolver {
+    fn take_type<F>(&mut self, sym: &JsWord, pred: F) -> Option<Type>
+    where
+        F: FnMut(&Type) -> bool,
+    {
+        if let Some(types) = self.info.types.get_mut(sym) {
+            let pos = types.iter().position(pred);
+            if let Some(pos) = pos {
+                return Some(types.remove(pos));
+            }
+        }
+
+        None
+    }
+}
 
 impl Fold<VarDecl> for TypeResolver {
     fn fold(&mut self, node: VarDecl) -> VarDecl {
@@ -72,9 +88,28 @@ impl Fold<ClassDecl> for TypeResolver {
 
 impl Fold<FnDecl> for TypeResolver {
     fn fold(&mut self, node: FnDecl) -> FnDecl {
+        if node.function.return_type.is_some() {
+            return node;
+        }
+
+        let node: FnDecl = node.fold_children(self);
+
+        let return_type = if let Some(ty) = self.take_type(&node.ident.sym, |ty| match ty {
+            Type::Function(..) => true,
+            _ => false,
+        }) {
+            Some(ty.into())
+        } else {
+            None
+        };
+
         FnDecl {
             declare: true,
-            ..node.fold_children(self)
+            function: Function {
+                return_type,
+                ..node.function
+            },
+            ..node
         }
     }
 }
