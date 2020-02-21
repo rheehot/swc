1b2637c3ab95d7e159a3448bb91a585f0307c70c
강동윤 <kdy1997.dev@gmail.com>
2020-02-21 15:42:41 +0900

    checker: PropertyMap

--- typescript/checker/src/util/property_map.rs
+++ typescript/checker/src/util/property_map.rs
@@ -8,7 +8,7 @@ use swc_ecma_ast::{ComputedPropName, Expr, Ident, Prop, PropName, Str};
 /// **Note**: this struct ignores span of key.
 #[derive(Debug)]
 pub struct PropertyMap<V> {
-    inner: Vec<(PropName, V)>,
+    inner: FxHashMap<PropName, V>,
 }
 impl<V> Default for PropertyMap<V> {
     fn default() -> Self {
@@ -20,35 +20,35 @@ impl<V> Default for PropertyMap<V> {
 
 impl<V> PropertyMap<V> {
     pub fn get(&self, expr: &Expr) -> Option<&V> {
+        println!("expr");
+
         let expr = PropName::Computed(ComputedPropName {
             span: DUMMY_SP,
             expr: box expr.clone().fold_with(&mut super::SpanRemover),
         });
 
-        for (k, v) in &self.inner {
-            if *k == expr {
-                return Some(&v);
-            }
-        }
-
-        None
+        self.inner.get(&expr)
     }
 
-    pub fn get_prop_anme(&self, p: &PropName) -> Option<&V> {
-        let expr = p.clone().fold_with(&mut super::SpanRemover);
+    pub fn get_prop_name(&self, p: &PropName) -> Option<&V> {
+        let p = p.clone().fold_with(&mut super::SpanRemover);
 
-        for (k, v) in &self.inner {
-            if *k == expr {
-                return Some(&v);
-            }
+        println!("p: {:?}", p);
+
+        if let Some(v) = self.inner.get(&p) {
+            println!("Matched");
+            Some(v)
+        } else {
+            println!("Not matched");
+
+            None
         }
-
-        None
     }
 
     pub fn insert(&mut self, key: PropName, v: V) {
         let key = key.fold_with(&mut super::SpanRemover);
-        self.inner.push((key, v));
+        println!("insert: {:?}", key);
+        self.inner.insert(key, v);
     }
 use swc_ecma_ast::{Expr, Ident, Str};
 use swc_common::DUMMY_SP;
