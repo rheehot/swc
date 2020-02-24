064bc97b77e1a3c9208406187fbe10dfb7109a39
강동윤 <kdy1997.dev@gmail.com>
2020-02-24 21:36:33 +0900

    Change type parameter name in a test

--- typescript/dts/tests/fixtures/types/conditional/infer/test-2/index.ts
+++ typescript/dts/tests/fixtures/types/conditional/infer/test-2/index.ts
@@ -1,7 +1,11 @@
 export type BadNested<T> = { x: T extends number ? T : string };
 
-export declare function foo2<T>(obj: T): T extends { [K in keyof BadNested<infer P>]: BadNested<infer P>[K] } ? P : never;
+export declare function foo2<T1>(
+  obj: T1
+): T1 extends { [K in keyof BadNested<infer P>]: BadNested<infer P>[K] }
+  ? P
+  : never;
 
-export function bar2<T>(obj: T) {
-    return foo2(obj);
+export function bar2<T2>(obj: T2) {
+  return foo2(obj);
 }
