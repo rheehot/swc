a22f8f4ad980fe689232e4e617fc8a0220d9dcf5
강동윤 <kdy1997.dev@gmail.com>
2020-02-26 12:06:51 +0900

    Split a test

---
+++ typescript/dts/tests/fixtures/types/mapped/conformance-4/test-2.ts
@@ -0,0 +1,23 @@
+type A = { a: string };
+type B = { b: string };
+type C = { c: string };
+
+function f1(x: A | B | C | undefined) {
+    return boxify(x);
+}
+
+type T00 = Partial<A | B | C>;
+type T01 = Readonly<A | B | C | null | undefined>;
+type T02 = Boxified<A | B[] | C | string>
+type T03 = Readonly<string | number | boolean | null | undefined | void>;
+type T04 = Boxified<string | number | boolean | null | undefined | void>;
+type T05 = Partial<"hello" | "world" | 42>;
+
+
+type BoxifiedWithSentinel<T, U> = {
+    [P in keyof T]: Box<T[P]> | U;
+}
+
+type T10 = BoxifiedWithSentinel<A | B | C, null>;
+type T11 = BoxifiedWithSentinel<A | B | C, undefined>;
+type T12 = BoxifiedWithSentinel<string, undefined>;
