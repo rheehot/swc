5bb450a2e14935ed5b2d0b83048f35e2a4ca996e
강동윤 <kdy1997.dev@gmail.com>
2020-02-27 19:08:30 +0900

    Rename type parameters

--- typescript/dts/tests/fixtures/types/generic/simple/test-11.ts
+++ typescript/dts/tests/fixtures/types/generic/simple/test-11.ts
@@ -1,3 +1,3 @@
-const arrayMap = <T, U>(f: (x: T) => U) => (a: T[]) => a.map(f);
+const arrayMap = <A, B>(f: (x: A) => B) => (a: A[]) => a.map(f);
 
 const f20: (a: string[]) => number[] = arrayMap(x => x.length);
