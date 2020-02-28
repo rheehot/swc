3308d294483d176e4d9454c3b75eec88352141cf
강동윤 <kdy1997.dev@gmail.com>
2020-02-28 22:59:46 +0900

    Fix test

--- typescript/dts/tests/fixtures/types/mapped/conformance-4/test-2.ts
+++ typescript/dts/tests/fixtures/types/mapped/conformance-4/test-2.ts
@@ -1,3 +1,21 @@
+type Box<T> = {
+};
+
+type Boxified<T> = {
+    [P in keyof T]: Box<T[P]>;
+};
+
+function boxify<T>(obj: T): Boxified<T> {
+    if (typeof obj === "object") {
+        let result = {} as Boxified<T>;
+        for (let k in obj) {
+            result[k] = { value: obj[k] };
+        }
+        return result;
+    }
+    return <any>obj;
+}
+
 type A = { a: string };
 type B = { b: string };
 type C = { c: string };
