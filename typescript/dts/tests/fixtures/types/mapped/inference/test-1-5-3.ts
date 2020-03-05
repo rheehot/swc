3af9cc89c1305c7339878ccbb0d4f51359e4cc69
강동윤 <kdy1997.dev@gmail.com>
2020-03-05 23:49:34 +0900

    Split

--- typescript/dts/tests/fixtures/types/mapped/inference/test-1-5-3.ts
+++ typescript/dts/tests/fixtures/types/mapped/inference/test-1-5-3.ts
@@ -18,19 +18,9 @@ declare function boxify<T>(obj: T): Boxified<T>;
 
 declare function unboxify<T>(obj: Boxified<T>): T ;
 
-function assignBoxified<T>(obj: Boxified<T>, values: T) {
-    for (let k in values) {
-        obj[k].value = values[k];
-    }
-}
-
-function makeRecord<T, K extends string>(obj: { [P in K]: T }) {
-    return obj;
-}
-
-let b = makeRecord({
-    a: box(42),
-    b: box("hello"),
-    c: box(true)
-});
+declare let b: {
+    a: Box<number> | Box<string> | Box<boolean>;
+    b: Box<number> | Box<string> | Box<boolean>;
+    c: Box<number> | Box<string> | Box<boolean>;
+};
 let v = unboxify(b);
