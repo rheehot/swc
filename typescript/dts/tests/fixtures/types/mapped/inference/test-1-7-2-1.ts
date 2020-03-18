271a1bc7cea5115d2fef73a190f3002f7737bf7f
강동윤 <kdy1997.dev@gmail.com>
2020-03-18 15:03:10 +0900

    Split

---
+++ typescript/dts/tests/fixtures/types/mapped/inference/test-1-7-2-1.ts
@@ -0,0 +1,43 @@
+// @strictNullChecks: true
+// @noimplicitany: true
+// @declaration: true
+
+declare type Box<T> = {
+    value: T;
+};
+declare type Boxified<T> = {
+    [P in keyof T]: Box<T[P]>;
+};
+declare function box<T>(x: T): Box<T>;
+declare function unbox<T>(x: Box<T>): T;
+declare function boxify<T>(obj: T): Boxified<T>;
+declare function unboxify<T>(obj: Boxified<T>): T;
+declare function assignBoxified<T>(obj: Boxified<T>, values: T): void;
+
+declare function validate<T>(obj: { [P in keyof T]?: T[P] }): T;
+
+declare function clone<T>(obj: { readonly [P in keyof T]: T[P] }): T;
+
+declare function validateAndClone<T>(obj: { readonly [P in keyof T]?: T[P] }): T;
+
+// Repro from #12606
+
+type Func<T> = (...args: any[]) => T;
+type Spec<T> = {
+    [P in keyof T]: Func<T[P]> | Spec<T[P]>;
+};
+
+/**
+ * Given a spec object recursively mapping properties to functions, creates a function
+ * producing an object of the same structure, by mapping each property to the result
+ * of calling its associated function with the supplied arguments.
+ */
+declare function applySpec<T>(obj: Spec<T>): (...args: any[]) => T;
+
+// Infers g1: (...args: any[]) => { sum: number, nested: { mul: string } }
+var g1 = applySpec({
+    sum: (a: any) => 3,
+    nested: {
+        mul: (b: any) => "n"
+    }
+});
