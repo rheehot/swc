1e285c9851ae3aaecbe8224043960e9e3906a960
강동윤 <kdy1997.dev@gmail.com>
2020-03-18 14:59:01 +0900

    Smaller test

--- typescript/dts/tests/fixtures/types/mapped/inference/test-1-7.ts
+++ typescript/dts/tests/fixtures/types/mapped/inference/test-1-7.ts
@@ -2,43 +2,17 @@
 // @noimplicitany: true
 // @declaration: true
 
-type Box<T> = {
+declare type Box<T> = {
     value: T;
-}
-
-type Boxified<T> = {
+};
+declare type Boxified<T> = {
     [P in keyof T]: Box<T[P]>;
-}
-
-function box<T>(x: T): Box<T> {
-    return {value: x};
-}
-
-function unbox<T>(x: Box<T>): T {
-    return x.value;
-}
-
-function boxify<T>(obj: T): Boxified<T> {
-    let result = {} as Boxified<T>;
-    for (let k in obj) {
-        result[k] = box(obj[k]);
-    }
-    return result;
-}
-
-function unboxify<T>(obj: Boxified<T>): T {
-    let result = {} as T;
-    for (let k in obj) {
-        result[k] = unbox(obj[k]);
-    }
-    return result;
-}
-
-function assignBoxified<T>(obj: Boxified<T>, values: T) {
-    for (let k in values) {
-        obj[k].value = values[k];
-    }
-}
+};
+declare function box<T>(x: T): Box<T>;
+declare function unbox<T>(x: Box<T>): T;
+declare function boxify<T>(obj: T): Boxified<T>;
+declare function unboxify<T>(obj: Boxified<T>): T;
+declare function assignBoxified<T>(obj: Boxified<T>, values: T): void;
 
 declare function validate<T>(obj: { [P in keyof T]?: T[P] }): T;
 
