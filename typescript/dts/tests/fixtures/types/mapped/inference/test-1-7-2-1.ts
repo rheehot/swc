bdc5e7bf7c775e89e7c35999d1dbf929fb5176fd
강동윤 <kdy1997.dev@gmail.com>
2020-03-18 15:12:32 +0900

    Smaller test

--- typescript/dts/tests/fixtures/types/mapped/inference/test-1-7-2-1.ts
+++ typescript/dts/tests/fixtures/types/mapped/inference/test-1-7-2-1.ts
@@ -1,25 +1,3 @@
-// @strictNullChecks: true
-// @noimplicitany: true
-// @declaration: true
-
-declare type Box<T> = {
-    value: T;
-};
-declare type Boxified<T> = {
-    [P in keyof T]: Box<T[P]>;
-};
-declare function box<T>(x: T): Box<T>;
-declare function unbox<T>(x: Box<T>): T;
-declare function boxify<T>(obj: T): Boxified<T>;
-declare function unboxify<T>(obj: Boxified<T>): T;
-declare function assignBoxified<T>(obj: Boxified<T>, values: T): void;
-
-declare function validate<T>(obj: { [P in keyof T]?: T[P] }): T;
-
-declare function clone<T>(obj: { readonly [P in keyof T]: T[P] }): T;
-
-declare function validateAndClone<T>(obj: { readonly [P in keyof T]?: T[P] }): T;
-
 // Repro from #12606
 
 type Func<T> = (...args: any[]) => T;
