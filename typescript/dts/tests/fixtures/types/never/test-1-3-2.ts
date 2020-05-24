3ec1e65be599a11da70dbac78345ea1210485068
강동윤 <kdy1997.dev@gmail.com>
2020-05-24 12:05:09 +0900

    fixup! Split tests

--- typescript/dts/tests/fixtures/types/never/test-1-3-2.ts
+++ typescript/dts/tests/fixtures/types/never/test-1-3-2.ts
@@ -1,16 +1,3 @@
-function error1(message: string): never {
-    throw new Error(message);
-}
-
-function fail1() {
-    return error1("Something failed");
-}
-
-function check<T>(x: T | undefined) {
-    return x || error1("Undefined value");
-}
-
-
 function f1(x: string | number) {
     if (typeof x === "boolean") {
         x;  // never
