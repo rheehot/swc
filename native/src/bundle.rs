21836ed18375898f82635199592929f94cd3b82e
강동윤 <kdy1997.dev@gmail.com>
2020-08-02 16:22:32 +0900

    Comment out neon-related codes

--- native/src/bundle.rs
+++ native/src/bundle.rs
@@ -1,7 +1,5 @@
-use crate::JsCompiler;
 use anyhow::{bail, Error};
 use fxhash::FxHashMap;
-use neon::prelude::*;
 use serde::Deserialize;
 use spack::{
     load::Load,
@@ -29,140 +27,142 @@ struct StaticConfigItem {
     config: spack::config::Config,
 }
 
-struct BundleTask {
-    swc: Arc<swc::Compiler>,
-    config: ConfigItem,
-}
-
-impl Task for BundleTask {
-    type Output = FxHashMap<String, TransformOutput>;
-    type Error = Error;
-    type JsEvent = JsValue;
-
-    fn perform(&self) -> Result<Self::Output, Self::Error> {
-        let res = catch_unwind(AssertUnwindSafe(|| {
-            let bundler = spack::Bundler::new(
-                self.swc.clone(),
-                self.config
-                    .static_items
-                    .config
-                    .options
-                    .as_ref()
-                    .map(|options| options.clone())
-                    .unwrap_or_else(|| {
-                        serde_json::from_value(serde_json::Value::Object(Default::default()))
-                            .unwrap()
-                    }),
-                &self.config.resolver,
-                &self.config.loader,
-            );
-
-            let result = bundler.bundle(&self.config.static_items.config)?;
-
-            let result = result
-                .into_iter()
-                .map(|bundle| match bundle.kind {
-                    BundleKind::Named { name } | BundleKind::Lib { name } => {
-                        Ok((name, bundle.module))
-                    }
-                    BundleKind::Dynamic => bail!("unimplemented: dynamic code splitting"),
-                })
-                .map(|res| {
-                    res.and_then(|(k, m)| {
-                        // TODO: Source map
-                        let minify = self
-                            .config
-                            .static_items
-                            .config
-                            .options
-                            .as_ref()
-                            .map(|v| {
-                                v.config
-                                    .as_ref()
-                                    .map(|v| v.minify)
-                                    .flatten()
-                                    .unwrap_or(false)
-                            })
-                            .unwrap_or(false);
-
-                        let output =
-                            self.swc
-                                .print(&m, SourceMapsConfig::Bool(true), None, minify)?;
-
-                        Ok((k, output))
-                    })
-                })
-                .collect::<Result<_, _>>()?;
-
-            Ok(result)
-        }));
-
-        let err = match res {
-            Ok(v) => return v,
-            Err(err) => err,
-        };
-
-        if let Some(s) = err.downcast_ref::<String>() {
-            bail!("panic detected: {}", s);
-        }
-
-        bail!("panic detected")
-    }
-
-    fn complete(
-        self,
-        mut cx: TaskContext,
-        result: Result<Self::Output, Self::Error>,
-    ) -> JsResult<Self::JsEvent> {
-        match result {
-            Ok(v) => Ok(neon_serde::to_value(&mut cx, &v)?.upcast()),
-            Err(err) => cx.throw_error(format!("{:?}", err)),
-        }
-    }
-}
-
-pub(crate) fn bundle(mut cx: MethodContext<JsCompiler>) -> JsResult<JsValue> {
-    let c: Arc<Compiler>;
-    let this = cx.this();
-    {
-        let guard = cx.lock();
-        let compiler = this.borrow(&guard);
-        c = compiler.clone();
-    }
-
-    let undefined = cx.undefined();
-
-    let opt = cx.argument::<JsObject>(0)?;
-    let callback = cx.argument::<JsFunction>(1)?;
-    let static_items = neon_serde::from_value(&mut cx, opt.upcast())?;
-
-    let loader = opt
-        .get(&mut cx, "loader")?
-        .downcast::<JsFunction>()
-        .map(|f| {
-            let handler = EventHandler::new(&mut cx, undefined, f);
-            //
-            Box::new(spack::loaders::neon::NeonLoader {
-                swc: c.clone(),
-                handler,
-            }) as Box<dyn Load>
-        })
-        .unwrap_or_else(|_| {
-            Box::new(spack::loaders::swc::SwcLoader::new(
-                c.clone(),
-                Default::default(),
-            ))
-        });
-
-    BundleTask {
-        swc: c.clone(),
-        config: ConfigItem {
-            loader,
-            resolver: Box::new(NodeResolver) as Box<_>,
-            static_items,
-        },
-    }
-    .schedule(callback);
-
-    Ok(cx.undefined().upcast())
-}
+// struct BundleTask {
+//     swc: Arc<swc::Compiler>,
+//     config: ConfigItem,
+// }
+//
+// impl Task for BundleTask {
+//     type Output = FxHashMap<String, TransformOutput>;
+//     type Error = Error;
+//     type JsEvent = JsValue;
+//
+//     fn perform(&self) -> Result<Self::Output, Self::Error> {
+//         let res = catch_unwind(AssertUnwindSafe(|| {
+//             let bundler = spack::Bundler::new(
+//                 self.swc.clone(),
+//                 self.config
+//                     .static_items
+//                     .config
+//                     .options
+//                     .as_ref()
+//                     .map(|options| options.clone())
+//                     .unwrap_or_else(|| {
+//                         
+// serde_json::from_value(serde_json::Value::Object(Default::default()))
+//                             .unwrap()
+//                     }),
+//                 &self.config.resolver,
+//                 &self.config.loader,
+//             );
+//
+//             let result = bundler.bundle(&self.config.static_items.config)?;
+//
+//             let result = result
+//                 .into_iter()
+//                 .map(|bundle| match bundle.kind {
+//                     BundleKind::Named { name } | BundleKind::Lib { name } =>
+// {                         Ok((name, bundle.module))
+//                     }
+//                     BundleKind::Dynamic => bail!("unimplemented: dynamic code
+// splitting"),                 })
+//                 .map(|res| {
+//                     res.and_then(|(k, m)| {
+//                         // TODO: Source map
+//                         let minify = self
+//                             .config
+//                             .static_items
+//                             .config
+//                             .options
+//                             .as_ref()
+//                             .map(|v| {
+//                                 v.config
+//                                     .as_ref()
+//                                     .map(|v| v.minify)
+//                                     .flatten()
+//                                     .unwrap_or(false)
+//                             })
+//                             .unwrap_or(false);
+//
+//                         let output =
+//                             self.swc
+//                                 .print(&m, SourceMapsConfig::Bool(true),
+// None, minify)?;
+//
+//                         Ok((k, output))
+//                     })
+//                 })
+//                 .collect::<Result<_, _>>()?;
+//
+//             Ok(result)
+//         }));
+//
+//         let err = match res {
+//             Ok(v) => return v,
+//             Err(err) => err,
+//         };
+//
+//         if let Some(s) = err.downcast_ref::<String>() {
+//             bail!("panic detected: {}", s);
+//         }
+//
+//         bail!("panic detected")
+//     }
+//
+//     fn complete(
+//         self,
+//         mut cx: TaskContext,
+//         result: Result<Self::Output, Self::Error>,
+//     ) -> JsResult<Self::JsEvent> {
+//         match result {
+//             Ok(v) => Ok(neon_serde::to_value(&mut cx, &v)?.upcast()),
+//             Err(err) => cx.throw_error(format!("{:?}", err)),
+//         }
+//     }
+// }
+//
+// pub(crate) fn bundle(mut cx: MethodContext<JsCompiler>) -> JsResult<JsValue>
+// {     let c: Arc<Compiler>;
+//     let this = cx.this();
+//     {
+//         let guard = cx.lock();
+//         let compiler = this.borrow(&guard);
+//         c = compiler.clone();
+//     }
+//
+//     let undefined = cx.undefined();
+//
+//     let opt = cx.argument::<JsObject>(0)?;
+//     let callback = cx.argument::<JsFunction>(1)?;
+//     let static_items = neon_serde::from_value(&mut cx, opt.upcast())?;
+//
+//     let loader = opt
+//         .get(&mut cx, "loader")?
+//         .downcast::<JsFunction>()
+//         .map(|f| {
+//             let handler = EventHandler::new(&mut cx, undefined, f);
+//             //
+//             Box::new(spack::loaders::neon::NeonLoader {
+//                 swc: c.clone(),
+//                 handler,
+//             }) as Box<dyn Load>
+//         })
+//         .unwrap_or_else(|_| {
+//             Box::new(spack::loaders::swc::SwcLoader::new(
+//                 c.clone(),
+//                 Default::default(),
+//             ))
+//         });
+//
+//     BundleTask {
+//         swc: c.clone(),
+//         config: ConfigItem {
+//             loader,
+//             resolver: Box::new(NodeResolver) as Box<_>,
+//             static_items,
+//         },
+//     }
+//     .schedule(callback);
+//
+//     Ok(cx.undefined().upcast())
+// }
--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,14 +1,13 @@
 #![recursion_limit = "2048"]
 
-extern crate neon;
-extern crate neon_serde;
-extern crate path_clean;
-extern crate serde;
-extern crate swc;
+#[macro_use]
+extern crate napi;
+#[macro_use]
+extern crate napi_derive;
 
 use anyhow::Error;
 use backtrace::Backtrace;
-use neon::prelude::*;
+use napi::{Module, Result};
 use std::{env, panic::set_hook, sync::Arc};
 use swc::{Compiler, TransformOutput};
 use swc_common::{self, errors::Handler, FilePathMapping, SourceMap};
@@ -18,93 +17,100 @@ mod parse;
 mod print;
 mod transform;
 
-fn init(_cx: MethodContext<JsUndefined>) -> NeonResult<ArcCompiler> {
-    if cfg!(debug_assertions) || env::var("SWC_DEBUG").unwrap_or_else(|_| String::new()) == "1" {
-        set_hook(Box::new(|_panic_info| {
-            let backtrace = Backtrace::new();
-            println!("Backtrace: {:?}", backtrace);
-        }));
-    }
+register_module!(native, init_module);
 
-    let cm = Arc::new(SourceMap::new(FilePathMapping::empty()));
-
-    let handler = Arc::new(Handler::with_tty_emitter(
-        swc_common::errors::ColorConfig::Always,
-        true,
-        false,
-        Some(cm.clone()),
-    ));
-
-    let c = Compiler::new(cm.clone(), handler);
-
-    Ok(Arc::new(c))
-}
-
-pub fn complete_output<'a>(
-    mut cx: impl Context<'a>,
-    result: Result<TransformOutput, Error>,
-) -> JsResult<'a, JsValue> {
-    match result {
-        Ok(output) => Ok(neon_serde::to_value(&mut cx, &output)?),
-        Err(err) => cx.throw_error(format!("{:?}", err)),
-    }
-}
-
-pub type ArcCompiler = Arc<Compiler>;
-
-declare_types! {
-    pub class JsCompiler for ArcCompiler {
-        init(cx) {
-            init(cx)
-        }
-
-        method transform(cx) {
-            transform::transform(cx)
-        }
-
-        method transformSync(cx) {
-            transform::transform_sync(cx)
-        }
-
-        method transformFile(cx) {
-            transform::transform_file(cx)
-        }
-
-        method transformFileSync(cx) {
-            transform::transform_file_sync(cx)
-        }
-
-        method parse(cx) {
-            parse::parse(cx)
-        }
-
-        method parseSync(cx) {
-            parse::parse_sync(cx)
-        }
-
-        method parseFile(cx) {
-            parse::parse_file(cx)
-        }
-
-        method parseFileSync(cx) {
-            parse::parse_file_sync(cx)
-        }
-
-        method print(cx) {
-            print::print(cx)
-        }
-
-        method printSync(cx) {
-            print::print_sync(cx)
-        }
-
-        method bundle(cx) {
-            bundle::bundle(cx)
-        }
-    }
-}
-
-register_module!(mut cx, {
-    cx.export_class::<JsCompiler>("Compiler")?;
+fn init_module(module: &mut Module) -> Result<()> {
+    module.create_named_method("transform", transform::transform)?;
     Ok(())
-});
+}
+
+// fn init(_cx: MethodContext<JsUndefined>) -> NeonResult<ArcCompiler> {
+//     if cfg!(debug_assertions) || env::var("SWC_DEBUG").unwrap_or_else(|_|
+// String::new()) == "1" {         set_hook(Box::new(|_panic_info| {
+//             let backtrace = Backtrace::new();
+//             println!("Backtrace: {:?}", backtrace);
+//         }));
+//     }
+//
+//     let cm = Arc::new(SourceMap::new(FilePathMapping::empty()));
+//
+//     let handler = Arc::new(Handler::with_tty_emitter(
+//         swc_common::errors::ColorConfig::Always,
+//         true,
+//         false,
+//         Some(cm.clone()),
+//     ));
+//
+//     let c = Compiler::new(cm.clone(), handler);
+//
+//     Ok(Arc::new(c))
+// }
+//
+// pub fn complete_output<'a>(
+//     mut cx: impl Context<'a>,
+//     result: Result<TransformOutput, Error>,
+// ) -> JsResult<'a, JsValue> {
+//     match result {
+//         Ok(output) => Ok(neon_serde::to_value(&mut cx, &output)?),
+//         Err(err) => cx.throw_error(format!("{:?}", err)),
+//     }
+// }
+//
+// pub type ArcCompiler = Arc<Compiler>;
+//
+// declare_types! {
+//     pub class JsCompiler for ArcCompiler {
+//         init(cx) {
+//             init(cx)
+//         }
+//
+//         method transform(cx) {
+//             transform::transform(cx)
+//         }
+//
+//         method transformSync(cx) {
+//             transform::transform_sync(cx)
+//         }
+//
+//         method transformFile(cx) {
+//             transform::transform_file(cx)
+//         }
+//
+//         method transformFileSync(cx) {
+//             transform::transform_file_sync(cx)
+//         }
+//
+//         method parse(cx) {
+//             parse::parse(cx)
+//         }
+//
+//         method parseSync(cx) {
+//             parse::parse_sync(cx)
+//         }
+//
+//         method parseFile(cx) {
+//             parse::parse_file(cx)
+//         }
+//
+//         method parseFileSync(cx) {
+//             parse::parse_file_sync(cx)
+//         }
+//
+//         method print(cx) {
+//             print::print(cx)
+//         }
+//
+//         method printSync(cx) {
+//             print::print_sync(cx)
+//         }
+//
+//         method bundle(cx) {
+//             bundle::bundle(cx)
+//         }
+//     }
+// }
+//
+// register_module!(mut cx, {
+//     cx.export_class::<JsCompiler>("Compiler")?;
+//     Ok(())
+// });
