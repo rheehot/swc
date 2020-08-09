4126c5cd934123afb710894dda41e2dce4d6f1cd
강동윤 <kdy1997.dev@gmail.com>
2020-08-09 20:38:51 +0900

    Found the reason

---
+++ visit/tests/opt_box.rs
@@ -0,0 +1,27 @@
+use std::any::Any;
+use swc_visit::define;
+
+/// Visitable nodes.
+pub trait Node: Any {}
+
+impl<T: ?Sized> Node for T where T: Any {}
+
+pub struct Item {
+    pub opt_box1: Option<Box<Item>>,
+    pub opt_box_conflict: Option<Item>,
+    pub opt_box2: Option<Box<Enum>>,
+}
+pub enum Enum {
+    Item(Item),
+}
+
+define!({
+    pub struct Item {
+        pub opt_box1: Option<Box<Item>>,
+        pub opt_box_conflict: Option<Item>,
+        pub opt_box2: Option<Box<Enum>>,
+    }
+    pub enum Enum {
+        Item(Item),
+    }
+});
